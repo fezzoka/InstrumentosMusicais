5d944807d49530c6fcdac48079503e88bf2de3b6
fezzoka <fezzoka@yahoo.com>
2024-10-07 23:36:34 -0300

    Criação Controller e view

--- controller/InstrumentController.java
+++ controller/InstrumentController.java
@@ -1,3 +1,38 @@
+package controller;
+
+import model.Instrument;
+import model.InstrumentDAO;
+import view.InstrumentView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class InstrumentController {
-    
+    private InstrumentView view;
+    private InstrumentDAO model;
+
+    public InstrumentController(InstrumentView view, InstrumentDAO model) {
+        this.view = view;
+        this.model = model;
+
+        view.addLoadButtonListener(new LoadInstrumentsListener());
+        view.addAddButtonListener(new AddInstrumentListener());
+    }
+
+    class LoadInstrumentsListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            view.displayInstruments(model.getAllInstruments());
+        }
+    }
+
+    class AddInstrumentListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String name = view.getInstrumentName();
+            double price = view.getInstrumentPrice();
+            model.addInstrument(new Instrument(0, name, price)); // ID gerado automaticamente
+            view.displayInstruments(model.getAllInstruments()); // Atualiza a lista
+        }
+    }
 }
--- view/InstrumentView.java
+++ view/InstrumentView.java
@@ -1,4 +1,61 @@
+package view;
+
+import model.Instrument;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.List;
+
 public class InstrumentView {
-    
+    private JFrame frame;
+    private JTextArea textArea;
+    private JTextField nameField;
+    private JTextField priceField;
+    private JButton loadButton;
+    private JButton addButton;
+
+    public InstrumentView() {
+        frame = new JFrame("Instrument Store");
+        textArea = new JTextArea(20, 50);
+        nameField = new JTextField(20);
+        priceField = new JTextField(10);
+        loadButton = new JButton("Load Instruments");
+        addButton = new JButton("Add Instrument");
+
+        frame.setLayout(new FlowLayout());
+        frame.add(new JLabel("Name:"));
+        frame.add(nameField);
+        frame.add(new JLabel("Price:"));
+        frame.add(priceField);
+        frame.add(addButton);
+        frame.add(loadButton);
+        frame.add(new JScrollPane(textArea));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void displayInstruments(List<Instrument> instruments) {
+        textArea.setText("");
+        for (Instrument instrument : instruments) {
+            textArea.append(instrument.getName() + " - $" + instrument.getPrice() + "\n");
+        }
+    }
+
+    public String getInstrumentName() {
+        return nameField.getText();
+    }
+
+    public double getInstrumentPrice() {
+        return Double.parseDouble(priceField.getText());
+    }
+
+    public void addLoadButtonListener(ActionListener listener) {
+        loadButton.addActionListener(listener);
+    }
+
+    public void addAddButtonListener(ActionListener listener) {
+        addButton.addActionListener(listener);
+    }
 }
-a
