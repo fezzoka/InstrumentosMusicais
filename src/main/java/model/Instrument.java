5c68a58808c395e2413b4aced02c5940ded86356
fezzoka <fezzoka@yahoo.com>
2024-10-07 23:28:35 -0300

    Criação model instrumento e configuração lista de instrumentos

--- src/main/java/model/Instrument.java
+++ src/main/java/model/Instrument.java
@@ -1,5 +1,22 @@
-package main.java.model;
+package model;
 
 public class Instrument {
-    
-}
+    private int id;
+    private String name;
+    private double price;
+
+    public Instrument(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    } // criação objeto instrumento
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setPrice(double price) { this.price = price; }
+}// config gett e sett
--- src/main/java/model/InstrumentDAO.java
+++ src/main/java/model/InstrumentDAO.java
@@ -1,5 +1,54 @@
-package main.java.model;
+package model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class InstrumentDAO {
-    
+    private Connection connection;
+
+    public InstrumentDAO() {
+        try {
+            Properties properties = new Properties();
+            properties.load(getClass().getResourceAsStream("/db.properties"));
+            String url = properties.getProperty("db.url");
+            String user = properties.getProperty("db.user");
+            String password = properties.getProperty("db.password");
+
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Instrument> getAllInstruments() {
+        List<Instrument> instruments = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM instruments";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                instruments.add(new Instrument(resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getDouble("price")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return instruments;
+    }
+
+    public void addInstrument(Instrument instrument) {
+        try {
+            String sql = "INSERT INTO instruments (name, price) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, instrument.getName());
+            preparedStatement.setDouble(2, instrument.getPrice());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
